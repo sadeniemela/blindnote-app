c763182e0073576d54b69144b62b82774f0947b7
Sade <sademarian@gmail.com>
2026-05-24 09:54:15 +0200

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Blindnote, anonymous feedback platform",
+  description: "Blindnote is an anonymous feedback platform that allows users to share their thoughts and opinions without revealing their identity. It provides a safe and secure environment for individuals to express themselves freely, fostering open communication and honest feedback.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
